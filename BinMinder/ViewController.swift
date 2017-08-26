1165c592cf7b150482563fadc4ffe74cab839570
Yousef <yezsef@me.com>
2017-08-26 18:37:58 +0100

    Can write to input box

--- BinMinder/ViewController.swift
+++ BinMinder/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    let webView = WKWebView()
+
+    @IBOutlet weak var postCodeTF: UITextField!
+    @IBOutlet weak var houseNoTF: UITextField!
+    
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://www.bromley.gov.uk/info/524/household_waste_and_recycling/961/bin_collection_days")!
+        let request = URLRequest(url: url)
+        
+        webView.frame = CGRect(x: 0, y: 300, width: 340, height: 200)
+        webView.load(request)
+        view.addSubview(webView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of xany resources that can be recreated.
     }
 
+    @IBAction func onRemindMePressed() {
+//        ID498503047
+        webView.evaluateJavaScript(
+//            "document.getElementById(\"ID498503047\").value = '\(postCodeTF.text!)'"
+            "document.getElementsByClassName(\"auroraAddressBox\")[0].value = '\(postCodeTF.text!)';",
+            completionHandler: { (value, error) in
+                print(value!)
+                print("Error: \(String(describing: error))")}
+        )
+    }
 
 }
 
